c8bc79eacb1dcf4ff2203fce8c24480e1fb8dd50
Aashutosh singh <ashutosh360a@gmail.com>
2025-07-25 18:35:17 +0530

    added the s3 part

--- terraform/main.tf
+++ terraform/main.tf
@@ -59,8 +59,11 @@ resource "aws_instance" "PublicInstance" {
   subnet_id   = aws_subnet.public.id
   associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name = "keypair"
-  user_data              = file("../scripts/user_data.sh")
+  user_data              = templatefile("${path.module}/../scripts/user_data.tmpl.sh", {
+  bucket_name = var.bucket_name
+})
   tags = {
     Name = "public instance 01"
   }
@@ -98,3 +101,164 @@ resource "aws_security_group" "allow_ssh_http" {
 
 ## s3 configuration
 
+//write only iam role
+resource "aws_iam_role" "s3_upload_only_role" {
+  name = "S3UploadOnlyRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+//s3 policy
+
+resource "aws_iam_role_policy" "s3_upload_only_policy" {
+  name = "S3UploadOnlyPolicy"
+  role = aws_iam_role.s3_upload_only_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:CreateBucket",
+          "s3:PutObjectAcl"
+        ]
+        Resource = [
+          "arn:aws:s3:::*",
+          "arn:aws:s3:::*/*"
+        ]
+      },
+      {
+        Effect = "Deny"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::*",
+          "arn:aws:s3:::*/*"
+        ]
+      }
+    ]
+  })
+}
+
+// s3 pollicy attachment
+
+# resource "aws_iam_role_policy_attachment" "writeonly_attach" {
+#     role = aws_iam_role.s3_upload_only_role
+#     policy_arn = aws_iam_policy.s3_upload_only_policy.arn
+  
+# }
+// profile configuration 
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "example-ec2-instance-profile"
+  role = aws_iam_role.s3_upload_only_role.name
+}
+// creating s3 bucket
+resource "aws_s3_bucket" "logs" {
+  bucket = var.bucket_name
+  force_destroy = true
+  tags = {
+    Name = "ec2-logs-bucket"
+  }
+}
+// ownership
+resource "aws_s3_bucket_ownership_controls" "ownership" {
+  bucket = aws_s3_bucket.logs.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+// lifecycle configuration 
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle_rule" {
+  bucket = aws_s3_bucket.logs.id
+  rule{
+    id = "delete-logs"
+    status = "Enabled"
+    expiration{
+      days = 7
+    }  
+    filter {
+      prefix = ""
+    }
+  }
+}
+
+
+//read only profile 
+
+resource "aws_iam_role" "s3_read_only_role" {
+  name = "S3ReadOnlyRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy" "s3_read_only_policy" {
+  name = "S3ReadOnlyPolicy"
+  role = aws_iam_role.s3_read_only_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Action = [
+        "s3:GetObject",
+        "s3:ListBucket"
+      ]
+      Resource = [
+        "arn:aws:s3:::*",
+        "arn:aws:s3:::*/*"
+      ]
+    }]
+  })
+}
+
+
+resource "aws_iam_instance_profile" "readonly_profile" {
+    name = "ec2-readonly-profile"
+    role = aws_iam_role.s3_read_only_role.name 
+}
+
+resource "aws_instance" "readonly_ec2" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  key_name      = "keypair"
+  vpc_security_group_ids = [aws_security_group.allow_ssh_http.id]
+  iam_instance_profile = aws_iam_instance_profile.readonly_profile.name
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y aws-cli
+              sleep 30
+              echo "Listing logs from S3..." > /home/ec2-user/readonly_check.log
+              sudo aws s3 ls s3://${var.bucket_name}/app/logs/ >> /home/ec2-user/readonly_check.log
+              sudo aws s3 ls s3://${var.bucket_name}/system/ >> /home/ec2-user/readonly_check.log
+              EOF
+
+  tags = {
+    Name = "techeazy-readonly-instance"
+  }   
+}  
